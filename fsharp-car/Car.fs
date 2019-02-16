c27012117aa02d994cea26817a3b91ceb809ce34
William <williamjlawrence1@gmail.com>
2019-02-15 22:49:22 -0500

    rearranged the files and added menus and user input

---
+++ fsharp-car/Car.fs
@@ -0,0 +1,14 @@
+﻿
+module Car
+
+open System
+open System.Text
+
+/// Drives to a given destination given a starting amount of petrol
+/// If they cant get there then it returns the orinal amount of petrol
+let driveTo (petrol, destination) = 
+    if destination = "p" then petrol + 10
+    elif destination = "Work" then petrol - 50
+    elif destination = "Home" then petrol - 25
+    elif destination = "Stadium" then petrol - 25
+    else petrol
---
+++ fsharp-car/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open Car
+
+/// Displays the possible destinations to the user and collects their input.
+/// This will always return the lowercase value
+let getDestination() =
+    Console.WriteLine("Please select a destination and press enter.")
+    Console.WriteLine("(H)ome")
+    Console.WriteLine("(W)ork")
+    Console.WriteLine("(S)tadium")
+    Console.WriteLine("(P)etrol Station")
+    let input = Console.ReadLine()
+    let destination = input.ToLower()
+    destination
+    
+let mutable petrol = 100
+
+[<EntryPoint>]
+let main argv =
+    while true do
+        try
+            let destination = getDestination()
+            printfn "Trying to drive to %s" destination
+            petrol <- driveTo(petrol, destination)
+            printfn "Made it to %s! You have %d petrol left" destination petrol
+        with ex -> printfn "ERROR: %s" ex.Message
+    0 // exit code that indicates that the program has ended properly.
